616289d64526acf151c9be2d2f89299b942c869e
Md Raziur Rahman <153851061+Raziur12@users.noreply.github.com>
2026-03-18 12:01:02 +0530

    The ESLint config issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignorePatterns: [
+    ignores: [
       ".next/**/*",
       "node_modules/**/*",
       "out/**/*",
